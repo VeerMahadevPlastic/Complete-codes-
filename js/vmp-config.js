092fcae3162c6fb464fdc770340c7d90e03cf942
veer mahadev plastic <veermahadevplastic@gmail.com>
2026-04-14 15:54:29 +0530

    Update vmp-config.js

--- js/vmp-config.js
+++ js/vmp-config.js
@@ -6,13 +6,13 @@
       razorpay: ''
     },
     firebase: {
-      apiKey: 'AIzaSyAGfZVese3tqFf_Uw_pJUQbSR5p6sohRmo',
-      authDomain: 'veermahadev--codes.firebaseapp.com',
-      projectId: 'veermahadev--codes',
-      storageBucket: 'veermahadev--codes.firebasestorage.app',
-      messagingSenderId: '380308449387',
-      appId: '1:380308449387:web:964942c3eb82a4c742292e',
-      measurementId: 'G-R2DHXNS9TF'
+      apiKey: '',
+      authDomain: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: '',
+      appId: '',
+      measurementId: ''
     },
     endpoints: {
       delhivery: 'https://track.delhivery.com/api/v1/packages/json/'
