a83bfdae02c0f4e71dc772a1d2971853b4b25822
veer mahadev plastic <veermahadevplastic@gmail.com>
2026-08-09 05:12:03 +0000

    Updated files

--- artifacts/vmp-store/src/app/products/[itemCode]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ProductDetail } from '../../components/product/ProductDetail';
-
-export default function ProductDetailPage({ params }: { params: { itemCode: string } }) {
-  return <ProductDetail itemCode={decodeURIComponent(params.itemCode)} />;
-}
